ad022674f6221bf701574cdc430aabd5aa2a0460
Bernhard Brodowsky <brodowsb@ethz.ch>
2014-10-19 22:27:42 +0200

    Order moves s.t. hash move is first.

--- olaf/search/moveorderer.cpp
+++ olaf/search/moveorderer.cpp
@@ -1,6 +1,7 @@
 #include "olaf/search/moveorderer.h"
 
 #include "olaf/transposition_table/transpositiontable.h"
+#include "olaf/search/searchcontext.h"
 
 using namespace std;
 
@@ -8,8 +9,21 @@ namespace olaf
 {
 
 // static
-void MoveOrderer::order_moves(const SearchContext& /* context */,
-                              vector<Move>* /* moves */)
-{}
+void MoveOrderer::order_moves(const SearchContext& context,
+                              vector<Move>* moves)
+{
+  const TranspositionTableEntry* const entry = context.get();
+  if (entry->has_best_move) {
+    for (Move& move : *moves) {
+      if (move.source() == entry->best_move_source
+          && move.destination() == entry->best_move_destination
+          && move.is_conversion() == entry->best_move_is_conversion
+          && (!move.is_conversion()
+              || move.created_piece() == entry->best_move_created_piece)) {
+        swap(move, moves->front());
+      }
+    }
+  }
+}
 
 } // namespace olaf
