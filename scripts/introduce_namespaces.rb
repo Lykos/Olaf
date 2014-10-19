daaebeb393b087d51c15b76189a0721e74ad29e9
Bernhard Brodowsky <brodowsb@ethz.ch>
2014-10-19 07:19:50 +0200

    Fixed namespace ends in script

--- scripts/introduce_namespaces.rb
+++ scripts/introduce_namespaces.rb
@@ -7,8 +7,12 @@ Dir.foreach('.') do |item|
   next unless item =~ /.+\.(h|cpp)/
   a = File.readlines(item)
   i = 0
-  while (a[i..-1].any? {|s| s =~ /(#include.*)|(using namespace.*)/})
+  j = -1
+  while (a[i..j].any? {|s| s =~ /(#include.*)|(using namespace.*)/})
     i += 1
+  end
+  while (a[i..j].any? {|s| s =~ /#endif\s+\/\/\s*\w*_H$/})
+    j -= 1
   end
   while (a[i] =~ /^\s*$/)
     i += 1
@@ -17,8 +21,9 @@ Dir.foreach('.') do |item|
     a[0...i].each { |l| f.puts l }
     namespaces.each { |l| f.puts "namespace #{l}"; f.puts "{" }
     f.puts ""
-    a[i..-1].each { |l| f.puts l }
+    a[i...j].each { |l| f.puts l }
     f.puts ""
     namespaces.reverse_each { |l| f.puts "} // namespace #{l}" }
+    a[j..-1].each { |l| f.puts l }
   end
 end
