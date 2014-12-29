143350b6b31ad760a066fa4cf3bee7914cac98f1
Bernhard Brodowsky <brodowsb@ethz.ch>
2014-12-30 00:23:23 +0100

    new games

---
+++ olaf/evaluation/compositeevaluator.cpp
@@ -0,0 +1,11 @@
+#include "compositeevaluator.h"
+
+using namespace std;
+
+namespace olaf
+{
+
+
+
+} // namespace olaf
+
---
+++ olaf/evaluation/compositeevaluator.h
@@ -0,0 +1,15 @@
+#ifndef COMPOSITEEVALUATOR_H
+#define COMPOSITEEVALUATOR_H
+
+namespace olaf
+{
+
+class CompositeEvaluator
+{
+public:
+};
+
+} // namespace olaf
+
+#endif // COMPOSITEEVALUATOR_H
+
